b993e5d9ba9343f427ec7f5fd87d83abe77e2692
AdilHussain2002 <2002.adil.hussain@gmail.com>
2026-01-14 15:36:15 +0000

    Entities have been filled

--- SpartaAPI/src/main/java/com/sparta/spartaapi/entities/Trainee.java
+++ SpartaAPI/src/main/java/com/sparta/spartaapi/entities/Trainee.java
@@ -1,4 +1,99 @@
 package com.sparta.spartaapi.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "trainees")
 public class Trainee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "trainee_id")
+    private Integer traineeID;
+
+    @Column(name = "full_name", nullable = false, length = 40)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 40)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "phone_number", length = 20)
+    private String phoneNumber;
+
+    @Column(name = "specialty_lang", length = 30)
+    private String specialtyLang;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    public Trainee() {}
+
+    public Trainee(Integer traineeID, String firstName, String lastName, String email, String phoneNumber, String specialtyLang) {
+        this.traineeID = traineeID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.specialtyLang = specialtyLang;
+    }
+
+    public Integer getTraineeID() {
+        return traineeID;
+    }
+
+    public void setTraineeID(Integer traineeID) {
+        this.traineeID = traineeID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSpecialtyLang() {
+        return specialtyLang;
+    }
+
+    public void setSpecialtyLang(String specialtyLang) {
+        this.specialtyLang = specialtyLang;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
 }
--- SpartaAPI/src/main/java/com/sparta/spartaapi/entities/Trainer.java
+++ SpartaAPI/src/main/java/com/sparta/spartaapi/entities/Trainer.java
@@ -1,4 +1,110 @@
 package com.sparta.spartaapi.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "trainers")
 public class Trainer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "trainer_id")
+    private Integer trainerID;
+
+    @Column(name = "full_name", nullable = false, length = 40)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 40)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "phone_number", length = 20)
+    private String phoneNumber;
+
+    @Column(name = "specialty_lang", length = 30)
+    private String specialtyLang;
+
+    @Column(name = "fav_animal", length = 30)
+    private String favAnimal;
+
+    @OneToOne(mappedBy = "trainer", fetch = FetchType.LAZY)
+    private Course course;
+
+    public Trainer() {}
+
+    public Trainer(Integer trainerID, String firstName, String lastName, String email, String phoneNumber, String specialtyLang, String favAnimal) {
+        this.trainerID = trainerID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.specialtyLang = specialtyLang;
+        this.favAnimal = favAnimal;
+    }
+
+    public Integer getTrainerID() {
+        return trainerID;
+    }
+
+    public void setTrainerID(Integer trainerID) {
+        this.trainerID = trainerID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSpecialtyLang() {
+        return specialtyLang;
+    }
+
+    public void setSpecialtyLang(String specialtyLang) {
+        this.specialtyLang = specialtyLang;
+    }
+
+    public String getFavAnimal() {
+        return favAnimal;
+    }
+
+    public void setFavAnimal(String favAnimal) {
+        this.favAnimal = favAnimal;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
 }
