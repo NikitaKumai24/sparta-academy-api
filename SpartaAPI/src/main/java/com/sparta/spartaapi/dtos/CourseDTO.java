e8ea5d3ed74c130c21ea71bdd4aa6cf94f27eab0
QueenEM <emmettbirch@hotmail.co.uk>
2026-01-15 09:28:24 +0000

    CourseDTO

--- SpartaAPI/src/main/java/com/sparta/spartaapi/dtos/CourseDTO.java
+++ SpartaAPI/src/main/java/com/sparta/spartaapi/dtos/CourseDTO.java
@@ -1,4 +1,68 @@
 package com.sparta.spartaapi.dtos;
 
+import java.time.LocalDate;
+
 public class CourseDTO {
+
+    private Integer courseId;
+
+    private Integer trainerId;
+
+    private String title;
+
+    private String description;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    // Getters && Setters
+
+    public Integer getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Integer courseId) {
+        this.courseId = courseId;
+    }
+
+    public Integer getTrainerId() {
+        return trainerId;
+    }
+
+    public void setTrainerId(Integer trainerId) {
+        this.trainerId = trainerId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
 }
