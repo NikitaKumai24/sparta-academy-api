f4f4e0b9ea9edc9fbceed9eee129cfb5acea7c68
Sameeksha <sameeksha.jalagari@gmail.com>
2026-01-15 12:07:58 +0000

    Methods added to Repository

--- SpartaAPI/src/main/java/com/sparta/spartaapi/repositories/TraineeRepository.java
+++ SpartaAPI/src/main/java/com/sparta/spartaapi/repositories/TraineeRepository.java
@@ -4,6 +4,17 @@ import com.sparta.spartaapi.entities.Trainee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TraineeRepository extends JpaRepository<Trainee, Integer> {
+
+    Optional<Trainee> findByName(String name);
+
+    List<Trainee> findByNameContainingIgnoreCase(String name);
+
+    List<Trainee> findByCourse(String course);
+
+
 }
